179647a51a4a7eaf307314ccff0f30cee86dd269
codalo <ckodalo@gmail.com>
2022-08-06 22:37:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('button', function() {
+        alert('I was clicked');
+
+    })
 }
+
